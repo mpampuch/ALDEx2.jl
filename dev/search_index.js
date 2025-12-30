675c37d004a42ca44be4de7e479eac3ee1f760a4
Documenter.jl <documenter@juliadocs.github.io>
2025-12-30 18:45:34 +0000

    build based on bfb0137

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ALDEx2","page":"Home","title":"ALDEx2","text":"Documentation for ALDEx2.\n\n","category":"section"}]
+[{"location":"#ALDEx2","page":"Home","title":"ALDEx2","text":"Documentation for ALDEx2.\n\n","category":"section"},{"location":"#ALDEx2.hello_world-Tuple{}","page":"Home","title":"ALDEx2.hello_world","text":"hello_world()\n\nPrint a greeting message. Used for testing package import to R.\n\n\n\n\n\n","category":"method"}]
 }
